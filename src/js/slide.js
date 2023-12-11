0a48be61b0396b00794c117d6c1e7f6a5c1317d0
Salando Linux <saulorlandozzz@gmail.com>
2023-12-11 17:02:00 -0600

    feat: se termin el responsive

--- src/js/slide.js
+++ src/js/slide.js
@@ -8,20 +8,20 @@ const toggle_section = () => {
   section_tanya.classList.toggle("active")
   if (section_tanya.classList.contains("active")) {
     section_john.style.position = "relative"
-    section_john.style.zIndex = "10"
     section_john.style.opacity = "100%"
     section_john.style.height = "100%"
+    section_john.style.zIndex = "10"
 
     section_tanya.style.opacity = "0"
     section_tanya.style.height = "0"
   } else {
-    section_tanya.style.opacity = "100%"
-    section_tanya.style.height = "100%"
-
     section_john.style.position = "absolute"
+    section_john.style.height = "0"
     section_john.style.zIndex = "-10"
     section_john.style.opacity = "0"
-    section_john.style.height = "0"
+
+    section_tanya.style.opacity = "100%"
+    section_tanya.style.height = "100%"
   }
 }
 
