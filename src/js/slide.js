c62fd9bbdcb0b4929862a5494d6ea0259e92b4d6
Salando Linux <saulorlandozzz@gmail.com>
2023-12-11 07:15:38 -0600

    feat: se termino de hacer el slide

---
+++ src/js/slide.js
@@ -0,0 +1,28 @@
+"use strict"
+
+const section_tanya = document.querySelector("#section-tanya")
+const section_john = document.querySelector("#section-john")
+const arrows = document.querySelectorAll(".arrow")
+
+const toggle_section = () => {
+  section_tanya.classList.toggle("active")
+  if (section_tanya.classList.contains("active")) {
+    section_john.style.zIndex = "10"
+    section_john.style.opacity = "100%"
+    section_john.style.height = "100%"
+
+    section_tanya.style.opacity = "0"
+    section_tanya.style.height = "0"
+  } else {
+    section_tanya.style.opacity = "100%"
+    section_tanya.style.height = "100%"
+
+    section_john.style.zIndex = "-10"
+    section_john.style.opacity = "0"
+    section_john.style.height = "0"
+  }
+}
+
+arrows.forEach(arrow => {
+  arrow.addEventListener("click", toggle_section)
+})
